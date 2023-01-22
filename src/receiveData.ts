e7af56c1dab71ff43cb05feef7c478e8ce5539b1
nico-fhnw <nico.heiniger@students.fhnw.ch>
2023-01-22 16:00:37 +0100

    adjusted formatting

--- src/receiveData.ts
+++ src/receiveData.ts
@@ -5,8 +5,7 @@ import {
   PublicAccount,
   RepositoryFactoryHttp,
   TransactionGroup,
-  TransferTransaction,
-  TransactionSearchCriteria
+  TransferTransaction
 } from '@dhealth/sdk';
 import { downloadFile } from "./downloadData";
 import fs from "fs";
@@ -16,50 +15,47 @@ const networkType = NetworkType.MAIN_NET;
 
 
 
-export const receiveData = async (transactionHash:string, researchPrivKey:string, patientPubKey:string): Promise<void> => {
+export const receiveData = async (transactionHash: string, researchPrivKey: string, patientPubKey: string): Promise<void> => {
 
 
-    const researchAccount = Account.createFromPrivateKey(
-      researchPrivKey,
-      networkType,
-    )
+  const researchAccount = Account.createFromPrivateKey(
+    researchPrivKey,
+    networkType,
+  )
 
-    const patientPublicAccount = PublicAccount.createFromPublicKey(
-      patientPubKey,
-      networkType,
-    )
+  const patientPublicAccount = PublicAccount.createFromPublicKey(
+    patientPubKey,
+    networkType,
+  )
 
-    const nodeUrl = 'http://api-01.dhealth.cloud:3000';
-    const repositoryFactory = new RepositoryFactoryHttp(nodeUrl);
-    const transactionHttp = repositoryFactory.createTransactionRepository();
+  const nodeUrl = 'http://api-01.dhealth.cloud:3000';
+  const repositoryFactory = new RepositoryFactoryHttp(nodeUrl);
+  const transactionHttp = repositoryFactory.createTransactionRepository();
 
-    transactionHttp.getTransaction(transactionHash, TransactionGroup.Confirmed).pipe(map((x) => x as TransferTransaction)).subscribe(
-        (transaction) => {
-          const decryptedMessage = researchAccount.decryptMessage(
-            transaction.message,
-            patientPublicAccount,
-          ).payload;
-            const ipfsHash = decryptedMessage.substring(0,decryptedMessage.search("/ehr")+4);
-            const decryptionKey = decryptedMessage.substring(decryptedMessage.search("/ehr")+4)
+  transactionHttp.getTransaction(transactionHash, TransactionGroup.Confirmed).pipe(map((x) => x as TransferTransaction)).subscribe(
+    (transaction) => {
+      const decryptedMessage = researchAccount.decryptMessage(
+        transaction.message,
+        patientPublicAccount,
+      ).payload;
+      const ipfsHash = decryptedMessage.substring(0, decryptedMessage.search("/ehr") + 4);
+      const decryptionKey = decryptedMessage.substring(decryptedMessage.search("/ehr") + 4)
 
 
 
-          downloadFile(ipfsHash).then(() => {
+      downloadFile(ipfsHash).then(() => {
 
-            const encryptedFile = fs.readFile('../receivedUtils/downloaded_encrypted_file.hl7', (err, file) => {
-              const decryptedFile = decrypt(file, decryptionKey);
-              fs.writeFile("../receivedUtils/decrypted_file.hl7", decryptedFile, () => {     
-
-                console.log("\n File successfully downloaded!");
-                const time4 = new Date().getTime();
-                console.log(time4);
-              })
-            })               
-
-            
-          }); 
-          }
-      )
+        fs.readFile('../receivedUtils/downloaded_encrypted_file.hl7', (err, file) => {
+          const decryptedFile = decrypt(file, decryptionKey);
+          fs.writeFile("../receivedUtils/decrypted_file.hl7", decryptedFile, () => {
+            console.log("\n File successfully downloaded!");
+            const time4 = new Date().getTime();
+            console.log(time4);
+          })
+        })
+      });
+    }
+  )
 
 }
 
