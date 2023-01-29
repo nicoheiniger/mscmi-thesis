506ae3aaaf7909588d9b8d807c5c99709e39b731
nico-fhnw <nico.heiniger@students.fhnw.ch>
2023-01-29 21:37:19 +0100

    added escrow transaction to return DHP to patient

--- src/sendData.ts
+++ src/sendData.ts
@@ -5,6 +5,12 @@ import {
     RepositoryFactoryHttp,
     TransferTransaction,
     UInt64,
+    Mosaic,
+    MosaicId,
+    PlainMessage,
+    AggregateTransaction,
+    TransactionService,
+    HashLockTransaction,
   } from '@dhealth/sdk';
 
   
@@ -15,6 +21,9 @@ import {
     const epochAdjustment = await repositoryFactory.getEpochAdjustment().toPromise();
 
     const networkType = await repositoryFactory.getNetworkType().toPromise();
+
+    const dhpMosaicId = new MosaicId('39E0C49FA322A459');
+    const dhpDivisibility = 6;
 
     const patientAccount = Account.createFromPrivateKey(
       patientPrivateKey,
@@ -40,19 +49,80 @@ import {
       UInt64.fromUint(50000),
     );
 
+    const returnTransaction = TransferTransaction.create(
+    Deadline.create(epochAdjustment),
+    patientAccount.address,
+    [
+      new Mosaic(
+        dhpMosaicId,
+        UInt64.fromUint(1 * Math.pow(10, dhpDivisibility)),
+      ),
+    ],
+    PlainMessage.create(""),
+    networkType,
+    UInt64.fromUint(50000),
+  );
+
+  const aggregateTransaction = AggregateTransaction.createBonded(
+    Deadline.create(epochAdjustment),
+    [
+      transferTransaction.toAggregate(patientAccount.publicAccount),
+      returnTransaction.toAggregate(researchPublicAccount),
+    ],
+    networkType,
+    [],
+    UInt64.fromUint(50000),
+  );
+
     const networkGenerationHash = await repositoryFactory.getGenerationHash().toPromise();
 
     const signedTransaction = patientAccount.sign(
-      transferTransaction,
+      aggregateTransaction,
       networkGenerationHash,
     );
     console.log("\n Your data was sent successfully with the transaction hash:",signedTransaction.hash);
 
+    const hashLockTransaction = HashLockTransaction.create(
+      Deadline.create(epochAdjustment),
+      new Mosaic(
+        dhpMosaicId,
+        UInt64.fromUint(10 * Math.pow(10, dhpDivisibility)),
+      ),
+      UInt64.fromUint(480),
+      signedTransaction,
+      networkType,
+      UInt64.fromUint(50000),
+    );
+
+    const signedHashLockTransaction = patientAccount.sign(
+      hashLockTransaction,
+      networkGenerationHash,
+    );
+
     const transactionRepository = repositoryFactory.createTransactionRepository();
-  
-    const response = await transactionRepository
+    const listener = repositoryFactory.createListener();
+    const receiptHttp = repositoryFactory.createReceiptRepository();
+    const transactionService = new TransactionService(transactionRepository, receiptHttp);
+    
+    listener.open().then(() => {
+      transactionService
+        .announceHashLockAggregateBonded(
+          signedHashLockTransaction,
+          signedTransaction,
+          listener,
+        )
+        .subscribe(
+          (x) => console.log(x),
+          (err) => console.log(err),
+          () => listener.close(),
+        );
+    });
+
+
+
+    /* const response = await transactionRepository
       .announce(signedTransaction)
-      .toPromise();
+      .toPromise(); */
     
 
   };
