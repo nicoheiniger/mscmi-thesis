2700394a54abd021616c97f4efcb00e77c39b6c5
nico-fhnw <nico.heiniger@students.fhnw.ch>
2023-01-30 11:53:54 +0100

    update signtransaction.ts to work as external function

--- src/signTransaction.ts
+++ src/signTransaction.ts
@@ -9,30 +9,20 @@ import {
   TransactionGroup,
 } from '@dhealth/sdk';
 
-/* start block 01 */
-const cosignAggregateBondedTransaction = (
-  transaction: AggregateTransaction,
-  account: Account,
-): CosignatureSignedTransaction => {
-  const cosignatureTransaction = CosignatureTransaction.create(transaction);
-  return account.signCosignatureTransaction(cosignatureTransaction);
-};
-/* end block 01 */
+export async function cosignTransaction(transactionHash: string, researchPrivKey: string) {
+  
+  const networkType = NetworkType.MAIN_NET;
 
-/* start block 02 */
-// replace with network type
-const networkType = NetworkType.TEST_NET;
-// replace with private key
-const privateKey =
-  '8E6A752344D2FD95877AEE92121CF3636F69566E9067961E40BC6B0B26669429';
-const account = Account.createFromPrivateKey(privateKey, networkType);
-// replace with node endpoint
-// replace with transaction hash to cosign
-const transactionHash =
-  '108DAC5410660208030FC7F8E43A6EC1290CB3B4815319D21841AD6953FF6163';
-/* end block 02 */
+  const cosignAggregateBondedTransaction = (
+    transaction: AggregateTransaction,
+    account: Account,
+  ): CosignatureSignedTransaction => {
+    const cosignatureTransaction = CosignatureTransaction.create(transaction);
+    return account.signCosignatureTransaction(cosignatureTransaction);
+  };
 
-/* start block 03 */
+  const account = Account.createFromPrivateKey(researchPrivKey, networkType);
+
 const nodeUrl = 'http://api-01.dhealth.cloud:3000';
 const repositoryFactory = new RepositoryFactoryHttp(nodeUrl);
 const transactionHttp = repositoryFactory.createTransactionRepository();
@@ -58,4 +48,5 @@ transactionHttp
     (announcedTransaction) => console.log(announcedTransaction),
     (err) => console.error(err),
   );
-/* end block 03 */
+
+} 
